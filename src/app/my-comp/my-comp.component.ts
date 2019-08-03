04f5a94edc4dd496fbe481ca622ab07ad5bfeec6
alahbabi <lahbabi.abdelkarim@gmail.com>
2019-08-03 10:53:42 +0200

    ALI : Intepolation

---
+++ src/app/my-comp/my-comp.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-comp',
+  templateUrl: './my-comp.component.html',
+  styleUrls: ['./my-comp.component.css']
+})
+export class MyCompComponent implements OnInit {
+
+  number = 5 ;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
