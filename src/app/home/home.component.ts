356e4a40825fabb6bb889ec7f6b11e4f71571f7a
alahbabi <lahbabi.abdelkarim@gmail.com>
2019-08-04 15:30:41 +0200

    ALI : Observables & Async Pipe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   myColor = '#080';
 
@@ -24,11 +25,44 @@ export class HomeComponent implements OnInit {
 
   showForm = false;
 
+  showObs = false;
+
   numberArray = [1, 3, 4 , 6];
 
   name;
 
   email;
+
+  myArr: Array<number> = [];
+
+  myO = Observable.create( (observer: Observer<string>) => {
+    setTimeout(() => {
+      observer.next('Hello');
+    }, 1000);
+    setTimeout(() => {
+      observer.error('error');
+    }, 4000);
+    setTimeout(() => {
+      observer.complete();
+    }, 6000);
+  });
+
+
+  myOTwo = Observable.create( (observer: Observer<string>) => {
+    setInterval(() => {
+      observer.next('Hello');
+    }, 1000);
+  });
+
+  myOThree: Observable<Array<number>> = Observable.create( (observer: Observer<Array<number>>) => {
+    let arr = [];
+    setInterval(() => {
+      arr.push(arr.length);
+      observer.next(arr);
+    }, 1000);
+  });
+
+  mySub: Subscription;
 
   @HostListener('click') onClickCallHostListener() {
     console.log('Clicked inside component');
@@ -37,6 +71,21 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.myO.subscribe((data) => {
+      console.log(data);
+    }, (error) => {
+      console.log(error);
+    }, () => {
+      console.log('complete');
+    });
+
+    this.mySub = this.myOTwo.subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  ngOnDestroy() {
+    this.mySub.unsubscribe();
   }
 
   addNumber() {
